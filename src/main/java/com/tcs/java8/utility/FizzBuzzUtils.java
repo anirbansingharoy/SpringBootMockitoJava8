b48aee4e0bc0aab01ef0d76b6a327b3e8f9a53c6
Anirban <anirban.singharoy@tcs.com>
2017-10-13 13:22:14 +0530

    Logic implemented to  as per Acceptance Criteria
    Acceptance Criteria is as follows
    
    Return "fizz", "buzz" or "fizzbuzz".
    
    For a given natural number greater than zero return:
    
    "fizz" if the number is dividable by 3
    "buzz" if the number is dividable by 5
    "fizzbuzz" if the number is dividable by 15
    the same number if no other requirement is fulfilled

--- src/main/java/com/tcs/java8/utility/FizzBuzzUtils.java
+++ src/main/java/com/tcs/java8/utility/FizzBuzzUtils.java
@@ -4,6 +4,13 @@ public class FizzBuzzUtils {
 
     public static String getFizzBuzzResponse(int inputNumber)
     {
+
+        if(inputNumber%3==0&&inputNumber%15!=0)
+            return "fizz";
+        else if (inputNumber%5==0&&inputNumber%15!=0)
+            return "buzz";
+        else if (inputNumber%15==0)
+            return "fizzbuzz";
         return  String.valueOf(inputNumber);
     }
 
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.StringUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class FizzBuzzTest {
@@ -12,5 +13,23 @@ public class FizzBuzzTest {
 {
     Assert.assertEquals("fizz", FizzBuzzUtils.getFizzBuzzResponse(9));
 }
+
+    @Test
+    public void WhenInputNumberDividedBy5ReturnBuzz()
+    {
+        Assert.assertEquals("buzz", FizzBuzzUtils.getFizzBuzzResponse(10));
+    }
+
+    @Test
+    public void WhenInputNumberDividedBy15ReturnFizzBuzz()
+    {
+        Assert.assertEquals("fizzbuzz", FizzBuzzUtils.getFizzBuzzResponse(30));
+    }
+    @Test
+    public void WhenInputNumberDoesNotDoesnotDividedby3or5or15ThenReturnSameNumber()
+    {
+        int inputNumber=101;
+        Assert.assertEquals(String.valueOf(inputNumber), FizzBuzzUtils.getFizzBuzzResponse(inputNumber));
+    }
 
 }
