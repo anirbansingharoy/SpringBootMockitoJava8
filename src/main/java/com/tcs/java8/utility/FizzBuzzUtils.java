5af40bde50f2a996a1a625775aa5061b6fbf610f
Anirban <anirban.singharoy@tcs.com>
2017-10-13 13:56:10 +0530

    Code Refactored and added logic to handle  Negetive Number Input

--- src/main/java/com/tcs/java8/utility/FizzBuzzUtils.java
+++ src/main/java/com/tcs/java8/utility/FizzBuzzUtils.java
@@ -2,15 +2,16 @@ package com.tcs.java8.utility;
 
 public class FizzBuzzUtils {
 
-    public static String getFizzBuzzResponse(int inputNumber)
+    public static String getFizzBuzzResponse(int inputNumber) throws RuntimeException
     {
-
-        if(inputNumber%3==0&&inputNumber%15!=0)
-            return "fizz";
-        else if (inputNumber%5==0&&inputNumber%15!=0)
-            return "buzz";
-        else if (inputNumber%15==0)
+      if(inputNumber<0)
+          throw new RuntimeException("NEGETIVE NUMBER IS NOT ALLOWED");
+        if (inputNumber%15==0)
             return "fizzbuzz";
+        else if(inputNumber%3==0)
+            return "fizz";
+        else if (inputNumber%5==0)
+            return "buzz";
         return  String.valueOf(inputNumber);
     }
 
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,6 +1,8 @@
 import com.tcs.java8.utility.FizzBuzzUtils;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.StringUtils;
@@ -8,28 +10,38 @@ import org.springframework.util.StringUtils;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class FizzBuzzTest {
 
-@Test
-    public void WhenInputNumberDividedBy3ReturnFizz()
-{
-    Assert.assertEquals("fizz", FizzBuzzUtils.getFizzBuzzResponse(9));
-}
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
 
     @Test
-    public void WhenInputNumberDividedBy5ReturnBuzz()
-    {
+    public void WhenInputNumberDividedBy3ReturnFizz() {
+        Assert.assertEquals("fizz", FizzBuzzUtils.getFizzBuzzResponse(9));
+    }
+
+    @Test
+    public void WhenInputNumberDividedBy5ReturnBuzz() {
         Assert.assertEquals("buzz", FizzBuzzUtils.getFizzBuzzResponse(10));
     }
 
     @Test
-    public void WhenInputNumberDividedBy15ReturnFizzBuzz()
-    {
+    public void WhenInputNumberDividedBy15ReturnFizzBuzz() {
         Assert.assertEquals("fizzbuzz", FizzBuzzUtils.getFizzBuzzResponse(30));
     }
+
     @Test
-    public void WhenInputNumberDoesNotDoesnotDividedby3or5or15ThenReturnSameNumber()
-    {
-        int inputNumber=101;
+    public void WhenInputNumberDoesNotDoesnotDividedby3or5or15ThenReturnSameNumber() {
+        int inputNumber = 101;
         Assert.assertEquals(String.valueOf(inputNumber), FizzBuzzUtils.getFizzBuzzResponse(inputNumber));
+    }
+
+    @Test
+    public void WhenNegetiveNumberIsPassedReturnException() {
+        try {
+            FizzBuzzUtils.getFizzBuzzResponse(-50);
+            Assert.fail("Expected Exception Actual No exception thrown");
+        } catch (RuntimeException ex) {
+            Assert.assertEquals("NEGETIVE NUMBER IS NOT ALLOWED", ex.getMessage());
+        }
     }
 
 }
